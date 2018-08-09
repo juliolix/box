f9b2e2e814be44658a6991162b0e6cb9fa57c606
juliolix <40389682+juliolix@users.noreply.github.com>
2018-08-09 10:27:38 -0300

    Update FireLix.sh

--- FireLix.sh
+++ FireLix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Firewall FireLix 
 #Be happy Joao :) uncle Julio Martins!
-#bash <(curl -s http://mywebsite.com/myscript.txt)
+#bash <(curl -s https://github.com/juliolix/box/blob/master/FireLix.sh)
 
 
 # Limpa todas as regras do Firewall
